65b5754f46592c0a36ff850a356d4d64164e8e64
AtikRahman <atik321@gmail.com>
2020-03-18 01:36:05 +0200

    redux-books-assignment

---
+++ src/store/action.js
@@ -0,0 +1,30 @@
+// action
+
+export const addOne = {
+  type: "ADD_ONE"
+};
+
+export const minusOne = {
+  type: "MINUS_ONE"
+};
+
+export const changeName = {
+  type: "CHANGE_NAME"
+  // payload: {
+  //   name: newName
+  // }
+};
+export const changeBackgroundColor = {
+  type: "CHANGE_COLOR"
+};
+
+export const getBooks = {
+  type: "GET_BOOKS"
+};
+
+export const selectBook = index => ({
+  type: "SELECT_BOOK",
+  payload: {
+    index: index
+  }
+});
